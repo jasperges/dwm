2c3f1d5300be02f6e93ab47b80ccc0a696d98f79
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2020-04-27 16:54:52 +0200

    Add statuspadding patch

--- config.h
+++ config.h
@@ -1,22 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 6;        /* horiz inner gap between windows */
-static const unsigned int gappiv    = 6;        /* vert inner gap between windows */
-static const unsigned int gappoh    = 0;        /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 0;        /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu Nerd Font:size=11:antialias=true" };
-static const char dmenufont[]       = "Ubuntu Nerd Font:size=11:antialias=true";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#285577";
+static const unsigned int borderpx    = 4;        /* border pixel of windows */
+static const unsigned int snap        = 32;       /* snap pixel */
+static const unsigned int gappih      = 6;        /* horiz inner gap between windows */
+static const unsigned int gappiv      = 6;        /* vert inner gap between windows */
+static const unsigned int gappoh      = 0;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov      = 0;        /* vert outer gap between windows and screen edge */
+static const int smartgaps            = 1;        /* 1 means no outer gap when there is only one window */
+static const int showbar              = 1;        /* 0 means no bar */
+static const int topbar               = 1;        /* 0 means bottom bar */
+static const int horizpadbar          = 0;        /* horizontal padding for statusbar */
+static const int vertpadbar           = 0;        /* vertical padding for statusbar */
+static const char *fonts[]            = { "Ubuntu Nerd Font:size=11:antialias=true" };
+static const char dmenufont[]         = "Ubuntu Nerd Font:size=11:antialias=true";
+static const char col_gray1[]         = "#222222";
+static const char col_gray2[]         = "#444444";
+static const char col_gray3[]         = "#bbbbbb";
+static const char col_gray4[]         = "#eeeeee";
+static const char col_cyan[]          = "#285577";
 static const unsigned int baralpha    = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
